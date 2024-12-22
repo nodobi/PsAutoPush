cfd38a73eaf2af742b3a2e3d6e2b312660f1b997
Dohyeok Kim <76645322+nodobi@users.noreply.github.com>
2024-12-22 15:30:31 +0900

    [Silver III] Title: N과 M (6), Time: 136 ms, Memory: 24152 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.kt
@@ -0,0 +1,41 @@
+package ps
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (N, M) = readLine().split(" ").map { it.toInt() }
+    val input = IntArray(N) { 0 }
+    val ans = IntArray(M) { 0 }
+    val isUsed = BooleanArray(N) { false }
+    val sb = StringBuilder()
+
+    readLine().split(" ").map { it.toInt() }.sorted().forEachIndexed { index, i ->
+        input[index] = i
+    }
+
+    fun sol(len: Int, cur: Int) {
+        if(len == M) {
+            for(i in 0 until M) {
+                sb.append("${ans[i]} ")
+            }
+            sb.append("\n")
+            return
+        }
+
+        for(i in 0 until N) {
+            if(!isUsed[i] && input[i] > cur) {
+                isUsed[i] = true
+                ans[len] = input[i]
+                sol(len + 1, input[i])
+                isUsed[i] = false
+            }
+        }
+    }
+
+    for(i in 0 until N) {
+        isUsed[i] = true
+        ans[0] = input[i]
+        sol(1, input[i])
+        isUsed[i] = false
+    }
+
+    print(sb.toString())
+}
